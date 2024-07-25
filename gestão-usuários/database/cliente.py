27a676f12a3bd3f6387b0e6f75bd1ef3beaea1bd
Keven Silveira <kevensilveira377@gmail.com>
2024-07-25 11:59:44 -0300

    Commit Pós-save

--- gestão-usuários/database/cliente.py
+++
@@ -1,8 +0,0 @@
-CLIENTES = [
-    {"id":1, "nome": "Kenshi", "email": "teste@teste.com"},
-    {"id":2, "nome": "Taliy", "email": "aaaa@teste.com"},
-    {"id":3, "nome": "Nelo", "email": "bbbb@teste.com"},
-    {"id":4, "nome": "Sailor", "email": "cccc@teste.com"},
-    {"id":5, "nome": "Nanda", "email": "dddd@teste.com"},
-    {"id":6, "nome": "Ana", "email": "eeee@teste.com"},
-]
--- gestão-usuários/routes/cliente.py
+++ gestão-usuários/routes/cliente.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, render_template, request
-from database.cliente import CLIENTES
 from database.models.cliente import Cliente
 
 cliente_route = Blueprint('cliente', __name__)
